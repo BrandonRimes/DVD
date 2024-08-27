149f6d31764c64a52bc1dc1b2f192f378308f7e1
BrandonRimesEFE <brandonrimes@Brandons-MacBook-Air.local>
2024-08-26 23:03:06 -0400

    oversight fix

--- script.js
+++ script.js
@@ -76,6 +76,8 @@ function toggleActiveContent(btn) {
   navName.style.marginTop = '1.5rem';
 
   if (btn === 'Home') {
+    contentContainer.style.background = 'transparent';
+
     navName.style.fontSize = '1rem';
     navName.style.letterSpacing = '1rem';
     navName.style.marginTop = '3rem';
@@ -134,45 +136,45 @@ window.onscroll = () => {
 
   // console.log(scroll);
 
-  if (scroll < 500) {
-    navName.style.fontSize = 1 - (scroll / 1000) + 'rem';
-    navName.style.letterSpacing = 1 - (scroll / 1000) + 'rem';
-    navName.style.marginTop = 3 - (scroll / 333) + 'rem';
-  } else {
-    navName.style.fontSize = '0.5rem';
-    navName.style.letterSpacing = '0.5rem';
-    navName.style.marginTop = '1.5rem';
-  }
+  // if (scroll < 500) {
+  //   navName.style.fontSize = 1 - (scroll / 1000) + 'rem';
+  //   navName.style.letterSpacing = 1 - (scroll / 1000) + 'rem';
+  //   navName.style.marginTop = 3 - (scroll / 333) + 'rem';
+  // } else {
+  //   navName.style.fontSize = '0.5rem';
+  //   navName.style.letterSpacing = '0.5rem';
+  //   navName.style.marginTop = '1.5rem';
+  // }
 
-  if (scroll < 333) {
-    pistol.style.transform = `translateY(-${ scroll * 1.2 }px) rotate(-90deg)`;
-  } else if (scroll < 600) {
-    pistol.style.transform = `translateY(-400px) rotate(${ -90 + ((scroll - 333) / 3) }deg)`;
-  } else {
-    pistol.style.transform = `translateY(-400px)`;
-  }
+  // if (scroll < 333) {
+  //   pistol.style.transform = `translateY(-${ scroll * 1.2 }px) rotate(-90deg)`;
+  // } else if (scroll < 600) {
+  //   pistol.style.transform = `translateY(-400px) rotate(${ -90 + ((scroll - 333) / 3) }deg)`;
+  // } else {
+  //   pistol.style.transform = `translateY(-400px)`;
+  // }
 
-  if (scroll > 700) {
-    bullet.style.transform = `translateX(-${ (scroll - 700) * 1.1 }px)`;
-  } else {
-    bullet.style.transform = `translateX(0px)`;
-  }
+  // if (scroll > 700) {
+  //   bullet.style.transform = `translateX(-${ (scroll - 700) * 1.1 }px)`;
+  // } else {
+  //   bullet.style.transform = `translateX(0px)`;
+  // }
 
-  if (scroll > 700 && scroll < 750) {
-    slide.style.transform = `translateX(${ (scroll - 700) }px) rotate(-0.66deg) translateY(1px)`;
-  } else if (scroll >= 750 && scroll < 800) {
-    slide.style.transform = `translateX(${ 50 + (750 - scroll) }px) rotate(-0.66deg) translateY(1px)`;
-  } else {
-    slide.style.transform = `translateX(0px) rotate(-0.66deg) translateY(1px)`;
-  }
+  // if (scroll > 700 && scroll < 750) {
+  //   slide.style.transform = `translateX(${ (scroll - 700) }px) rotate(-0.66deg) translateY(1px)`;
+  // } else if (scroll >= 750 && scroll < 800) {
+  //   slide.style.transform = `translateX(${ 50 + (750 - scroll) }px) rotate(-0.66deg) translateY(1px)`;
+  // } else {
+  //   slide.style.transform = `translateX(0px) rotate(-0.66deg) translateY(1px)`;
+  // }
 
-  if (scroll > 900 && scroll < 1175) {
-    pistol.style.transform = `translateY(-400px) rotate(${(900 - scroll) / 3 }deg)`;
-  } else if (scroll >= 1175 && scroll < 1575) {
-    pistol.style.transform = `translateY(-${ 400 - (scroll - 1175) }px) rotate(-90deg)`;
-  } else if (scroll >= 1575) {
-    pistol.style.transform = `translateY(0px) rotate(-90deg)`;
-  }
+  // if (scroll > 900 && scroll < 1175) {
+  //   pistol.style.transform = `translateY(-400px) rotate(${(900 - scroll) / 3 }deg)`;
+  // } else if (scroll >= 1175 && scroll < 1575) {
+  //   pistol.style.transform = `translateY(-${ 400 - (scroll - 1175) }px) rotate(-90deg)`;
+  // } else if (scroll >= 1575) {
+  //   pistol.style.transform = `translateY(0px) rotate(-90deg)`;
+  // }
 
   // console.log(slide.style.transform);
   // holster.style.transform = `translateY(${ scroll / 2 }px)`;
